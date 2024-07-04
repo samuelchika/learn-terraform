d7d5d4bd7051c56470f48540aa51246e746413fc
Samuel Ezeja <samez@localhost.localdomain>
2024-07-04 19:23:16 -0400

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -3,13 +3,13 @@
 
 terraform {
 
-  cloud {
-    organization = "organization-name"
+  # cloud {
+  #   organization = "TutsLearn"
 
-    workspaces {
-      name = "learn-terraform"
-    }
-  }
+  #   workspaces {
+  #     name = "learn-terraform"
+  #   }
+  # }
 
   required_providers {
     aws = {
